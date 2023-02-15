163b21c3c1b2b571dd846482bc60c8050fc636fa
bart1995 <55354809+bart1995@users.noreply.github.com>
2023-02-15 13:31:09 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,17 @@ import СountDim
 import RecDB
 
 def main():
+	# Подключаем библиотеки Компас API
 	Module7, KompasDocument2D = СountDim.plug_kompas()
+	# Считываем размеры
 	Dimensions = СountDim.dimensions_read(Module7, KompasDocument2D)
+	# Вызываем окно для указания пути сохранения excel-файла
 	PathXLXS = RecEx.path_window_xlsx()
+	# Сохраняем excel-файл
 	RecEx.write_book(Dimensions, PathXLXS)
+	# Вызываем окно для указания пути сохранения SQLite-файла
 	PathDB = RecDB.path_window_DB()
+	# Сохраняем Базу данных
 	RecDB.db_read(Dimensions, PathDB)
 
 
